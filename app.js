8d3ae35bb0862165a533e0330f93aa7fea81d963
danteteruk <danuloteteruk@gmail.com>
2016-03-10 23:31:04 +0200

    endless

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 //For avoidong Heroku $PORT error
-app.get('/', function(req, res) {
-    Article.find(function(err, article) {
+app.get('/', function(request, response) {
+  Article.find(function(err, article) {
     if (err)
       res.send(err);
 
